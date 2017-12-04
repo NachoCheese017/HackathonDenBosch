b1da769f2556e4dfcaa3553ec67cee46b1c44fc1
stijnklomp <32292824+stijnklomp@users.noreply.github.com>
2017-12-04 14:24:01 +0100

    functions
    DB aanroep functies

---
+++ functions.php
@@ -0,0 +1,64 @@
+<?php
+// Select from database
+function selectDatabase($pdo, $tableName, $whereValue, $whereKey, $addon)
+{
+	$query = 'SELECT * FROM '.$tableName;
+	if(!empty($whereValue))
+	{
+		$query .= ' WHERE '.$whereValue.' = "'.$whereKey.'" ';
+	}
+	$sth = $pdo->prepare($query.$addon);
+	$sth->execute();
+	return $sth;
+}
+
+// Insert into database
+function insertDatabase($pdo, $tableName, $arrayValues)
+{
+	$query = 'INSERT INTO '.$tableName.' SET ';
+	$i = 0;
+	foreach($arrayValues as $key => $value)
+	{
+		if($i != 0)
+		{
+			$query .= ', ';
+		}
+		$query .= $key.' = "'.$value.'"';
+		$i++;
+	}
+	$sth = $pdo->prepare($query);
+	$sth->execute();
+}
+
+// Update the database
+function updateDatabase($pdo, $tableName, $whereValue, $whereKey, $arrayValues)
+{
+	$query = 'UPDATE '.$tableName.' SET ';
+	$i = 0;
+	foreach($arrayValues as $key => $value)
+	{
+		if($i != 0)
+		{
+			$query .= ', ';
+		}
+		$query .= $key.' = "'.$value.'"';
+		$i++;
+	}
+	$query .= 'WHERE '.$whereValue.' = "'.$whereKey.'"';
+	$sth = $pdo->prepare($query);
+	$sth->execute();
+}
+
+// Delete from database
+function deleteDatabase($pdo, $tableName, $whereValue, $whereKey)
+{
+	$prepareParameters = array($parameters);
+	$query = 'DELETE FROM '.$tableName;
+	if(!empty($whereValue))
+	{
+		$query .= ' WHERE '.$whereValue.' = "'.$whereKey.'"';
+	}
+	$sth = $pdo->prepare($query);
+	$sth->execute($prepareParameters);
+}
+?>
