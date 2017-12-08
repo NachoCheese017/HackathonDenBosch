94566d969d34fa397e391cf4d9079bd41598ccf6
messenwerper <guusurselmann@gmail.com>
2017-12-08 16:47:29 +0100

    market view
    marketview

--- content/public/views/market-view.php
+++ content/public/views/market-view.php
@@ -1,6 +1,17 @@
 <div class="page_wrapper">
-    <!-- <div class="filterbar"> -->
-    <!-- </div> -->
+    <div class="filterbar">
+    </div>
+    <div class="productsfield">
+        <div class="products-filter">
+            <div class="searchbar">
+                <form action="market">
+                    <input type="text" placeholder="search..." name="product_search_input">
+                    <input type="submit" name="product_search_submit">
+                </form>
+            </div>
+        </div>
+        <div class="products-field">
+            <?php productDisplay($pdo); ?>
+        </div>
+    </div>
 </div>
-<?php
-productDisplay($pdo);
