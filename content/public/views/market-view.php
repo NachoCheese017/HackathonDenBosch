1e2687b91a56c553fac38cd8895df2cc0e518159
messenwerper <guusurselmann@gmail.com>
2017-12-08 19:19:52 +0100

    market update
    market update

--- content/public/views/market-view.php
+++ content/public/views/market-view.php
@@ -2,22 +2,25 @@
     <div class="filterbar">
     </div>
     <div class="productsfield">
-        <div class="products-filter">
-            <div class="searchbar">
-                <form action="">
-                    <input type="text" placeholder="search..." name="product_search_input">
-                    <input type="submit" name="product_search_submit">
-                </form>
-            </div>
-            <div class="showamount">
-                <form action="" method="post">
-                    <select name="changeTotalPages">
-                        <option value="15" <?php if($_SESSION['totalPages'] == 15){echo 'selected';} ?>>15</option>
-                        <option value="30" <?php if($_SESSION['totalPages'] == 30){echo 'selected';} ?>>30</option>
-                        <option value="45" <?php if($_SESSION['totalPages'] == 45){echo 'selected';} ?>>45</option>
-                    </select>
-                    <input type="submit" name="changeTotalPagesBtn" value="Apply">
-                </form>
+        <div class="product-search">
+            <div class="products-filter">
+                <div class="searchbar">
+                    <form action="">
+                        <input type="text" placeholder="zoeken..." name="product_search_input">
+                        <input type="submit" name="product_search_submit" value="zoek">
+                    </form>
+                </div>
+                <div class="showamount">
+                    <form action="" method="post">
+                        <label for="changeTotalPages">Aantal artikelen per pagina</label>
+                        <select name="changeTotalPages">
+                            <option value="15" <?php if($_SESSION['totalPages'] == 15){echo 'selected';} ?>>15</option>
+                            <option value="30" <?php if($_SESSION['totalPages'] == 30){echo 'selected';} ?>>30</option>
+                            <option value="45" <?php if($_SESSION['totalPages'] == 45){echo 'selected';} ?>>45</option>
+                        </select>
+                        <input type="submit" name="changeTotalPagesBtn" value="Opslaan">
+                    </form>
+                </div>
             </div>
         </div>
         <div class="products-field">
@@ -27,6 +30,3 @@
         </div>
     </div>
 </div>
-
-
-
