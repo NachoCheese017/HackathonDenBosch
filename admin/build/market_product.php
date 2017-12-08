6727d8f17d2b760ca05a399d215702efa7271c3b
messenwerper <guusurselmann@gmail.com>
2017-12-08 21:26:40 +0100

    s
    s

--- admin/build/market_product.php
+++ admin/build/market_product.php
@@ -1,3 +1,13 @@
-<div class="product-card">
-    hai
+<div class="product-card" id="productid_<?= $product['product_ID'] ?>">
+    <div class="card-content">
+        <div class="product-name">
+            <?= $product['p_type'].' '.$product['p_name'] ?>
+        </div>
+        <div class="">
+            <?= $product['p_brand'] ?>
+        </div>
+    </div>
+    <div class="readmore button2">
+        <a>Lees meer</a>
+    </div>
 </div>
