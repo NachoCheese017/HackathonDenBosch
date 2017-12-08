b4674684828369c9556a82f27fd8570ee9e8714a
messenwerper <guusurselmann@gmail.com>
2017-12-08 20:29:26 +0100

    home
    home

--- content/public/views/home-view.php
+++ content/public/views/home-view.php
@@ -1,16 +1,25 @@
 <div class="page_wrapper">
     <div class="home-banner">
-        <div class="button-market button2">
-            <a href="market">Markt</a>
+        <div class="button-left">
+            <div class="button-market button2">
+                <a href="market">Markt</a>
+            </div>
         </div>
-        <?php if(!loginCheck($pdo)) { ?>
-        <div class="button-register button2">
-            <a href="register">Aanmelden</a>
+        <div class="button-right">
+            <?php if(!loginCheck($pdo)) { ?>
+            <div class="button-register button2">
+                <a href="register">Aanmelden</a>
+            </div>
+            <?php } else { ?>
+            <div class="button-userprofile button2">
+                <a href="userprofile">Mijn profiel</a>
+            </div>
+            <?php } ?>
         </div>
-        <?php } else { ?>
-        <div class="button-userprofile button2">
-            <a href="userprofile">Mijn profiel</a>
+    </div>
+    <div class="company">
+        <div class="company-name">
+            SCHRIJVERS
         </div>
-        <?php } ?>
     </div>
 </div>
